60b1cdd5f9c64636baf39f76830ccf7719bd05cf
Uanderson Ricardo <uandersonrfs@gmail.com>
2023-05-03 23:59:13 -0300

    fix api for deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,4 +34,6 @@ app.use(
   }
 );
 
-app.listen(3333, () => console.log("Server running on port 3333"));
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => console.log("Server running on port " + port));
